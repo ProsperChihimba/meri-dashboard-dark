5205b120a7ed9b63b15ce130d486517c4ef2bf57
ProsperChihimba <prosperchihimba@gmail.com>
2022-09-01 02:55:01 +0300

    Update: Add all main cards

--- src/components/main/cards/index.jsx
+++ src/components/main/cards/index.jsx
@@ -8,7 +8,7 @@ import {
     Center,
     Button
 } from '@chakra-ui/react'
-import { FiUsers, FiTrendingDown } from 'react-icons/fi';
+import { FiUsers, FiTrendingDown, FiTrendingUp } from 'react-icons/fi';
 
 const Cards = () => {
     return (
@@ -42,9 +42,61 @@ const Cards = () => {
                     </Flex>
                 </Box>
                 <Spacer />
-                <Box>Hey</Box>
+                <Box w={260} h={150} bg='linear-gradient(119.15deg, #2E374B -13.54%, #292F3C 118.09%)' rounded='3xl'>
+                    <Flex m={5} direction='column'>
+                        <Flex w='100%' mb={5}>
+                            <Avatar size='sm' bg='teal.500' icon={<FiUsers fontSize='1.2rem' />} />
+                            <Spacer />
+                            <Flex direction='column'>
+                                <Center>
+                                    <FiTrendingUp color='teal' height={15} />
+                                    <Text ml={3} as='b' color='white' >3.3%</Text>
+                                </Center>
+                                <Text color='#B6B7BC' fontSize={12}>This month</Text>
+                            </Flex>
+                        </Flex>
+                        <Flex w='100%'>
+                            <Flex direction='column'>
+                                <Text color='#B6B7BC' fontSize={12}>Total earning</Text>
+                                <Text as='b' color='white' >1000K</Text>
+                            </Flex>
+                            <Spacer />
+                            <Center>
+                                <Button colorScheme='teal' size='sm'>
+                                    See info
+                                </Button>
+                            </Center>
+                        </Flex>
+                    </Flex>
+                </Box>
                 <Spacer />
-                <Box>Hey</Box>
+                <Box w={260} h={150} bg='linear-gradient(119.15deg, #2E374B -13.54%, #292F3C 118.09%)' rounded='3xl'>
+                    <Flex m={5} direction='column'>
+                        <Flex w='100%' mb={5}>
+                            <Avatar size='sm' bg='teal.500' icon={<FiUsers fontSize='1.2rem' />} />
+                            <Spacer />
+                            <Flex direction='column'>
+                                <Center>
+                                    <FiTrendingUp color='teal' height={15} />
+                                    <Text ml={3} as='b' color='white' >12.2%</Text>
+                                </Center>
+                                <Text color='#B6B7BC' fontSize={12}>This month</Text>
+                            </Flex>
+                        </Flex>
+                        <Flex w='100%'>
+                            <Flex direction='column'>
+                                <Text color='#B6B7BC' fontSize={12}>Total users</Text>
+                                <Text as='b' color='white' >1000</Text>
+                            </Flex>
+                            <Spacer />
+                            <Center>
+                                <Button colorScheme='teal' size='sm'>
+                                    See info
+                                </Button>
+                            </Center>
+                        </Flex>
+                    </Flex>
+                </Box>
             </Flex>
         </Box>
     )
